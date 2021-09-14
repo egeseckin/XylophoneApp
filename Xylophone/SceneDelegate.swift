eb80fdf90fef2dc998d99e60de6aa03f4c6aecd8
egeseckin <ege.seckin@outlook.com>
2021-09-14 13:56:52 +0300

    Name changed

--- Xylophone/SceneDelegate.swift
+++ Xylophone/SceneDelegate.swift
@@ -2,8 +2,8 @@
 //  SceneDelegate.swift
 //  Xylophone
 //
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Created by egeseckin on 30/08/2021.
+//  Copyright © 2021 Ege Seckin. All rights reserved.
 //
 
 import UIKit
